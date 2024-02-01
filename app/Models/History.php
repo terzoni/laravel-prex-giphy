2cab1cdabb59982e1ce10f67b5565ffc30d41f48
Marcos Terzoni <terzoni86@gmail.com>
2024-02-01 14:34:09 -0300

    Fix store history

--- app/Models/History.php
+++ app/Models/History.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 
 class History extends Model
 {
@@ -24,14 +25,14 @@ class History extends Model
     ];
 
     public static function store($request, $service, $response_code, $response_body) {
-        $user = Auth::user(); 
+        $user = Auth::user();
 
         $newHistory = new History();
         $newHistory->user_id = $user->id;
         $newHistory->service = ServiceNames::$names[$service];
         $newHistory->request_body = $request->getContent();
         $newHistory->response_http_code = $response_code;
-        $newHistory->response_body = json_encode($response_body);
+        $newHistory->response_body =  substr(json_encode($response_body), 0,1000);
         $newHistory->ip =  $request->ip();
         $newHistory->save();
     }
